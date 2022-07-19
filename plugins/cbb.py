a4bb371ff8824515c0705d8ff4ff81acb3bc71b3
mastermindmayank <88104889+mastermindmayankofficial@users.noreply.github.com>
2022-07-20 02:36:29 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(©)@mastermindmayankproject
 
 from pyrogram import __version__
 from bot import Bot
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>MASTERMIND MAYANK</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.instagram.com/themastermindmayank/'>Click here</a>\n○ Channel : @mastermindmayank2\n○ Support Group : @mastermindmayank0</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>MASTERMIND MAYANK</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://www.instagram.com/themastermindmayank/'>Click here</a>\n○ Channel : @mastermindmayankproject\n○ Support Group : @teammastermindmayank</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
